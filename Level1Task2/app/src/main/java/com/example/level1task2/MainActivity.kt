2dea6b70d11a3c9635a182d60ff8afe65cafb3ed
Teun Stout <60847859+teunstout@users.noreply.github.com>
2020-02-09 18:59:35 +0100

    checks correctly

--- Level1Task2/app/src/main/java/com/example/level1task2/MainActivity.kt
+++ Level1Task2/app/src/main/java/com/example/level1task2/MainActivity.kt
@@ -5,7 +5,6 @@ import android.os.Bundle
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 import kotlinx.android.synthetic.main.activity_main.view.*
-import java.lang.Exception
 
 class MainActivity : AppCompatActivity() {
 
@@ -21,40 +20,40 @@ class MainActivity : AppCompatActivity() {
 
     fun checkConjunction () {
         var correctAwnsers = 0
-        if (checkRowConjunction(conjunctionTable.row1.aRow1.text.toString() ,  conjunctionTable.row1.bRown1.text.toString())
-            .equals(checkAwnserRowConjunction(conjunctionTable.row1.abRow1.text.toString()))) correctAwnsers++
-
-        if (checkRowConjunction(conjunctionTable.row2.aRow2.text.toString() ,  conjunctionTable.row2.bRow2.text.toString())
-                .equals(checkAwnserRowConjunction(conjunctionTable.row2.abRow2.text.toString()))) correctAwnsers++
-
-        if (checkRowConjunction(conjunctionTable.row3.aRow3.text.toString() ,  conjunctionTable.row3.bRow3.text.toString())
-                .equals(checkAwnserRowConjunction(conjunctionTable.row3.abRow3.text.toString()))) correctAwnsers++
-
-        if (checkRowConjunction(conjunctionTable.row4.aRow4.text.toString() ,  conjunctionTable.row4.bRow4.text.toString())
-                .equals(checkAwnserRowConjunction(conjunctionTable.row4.abRow4.text.toString()))) correctAwnsers++
+        if (checkRowConjunction(conjunctionTable.row1.aRow1.text.toString(),  conjunctionTable.row1.bRow1.text.toString(), conjunctionTable.row1.abRow1.text.toString())) correctAwnsers++
+        if (checkRowConjunction(conjunctionTable.row2.aRow2.text.toString(),  conjunctionTable.row2.bRow2.text.toString(), conjunctionTable.row2.abRow2.text.toString())) correctAwnsers++
+        if (checkRowConjunction(conjunctionTable.row3.aRow3.text.toString() ,  conjunctionTable.row3.bRow3.text.toString(), conjunctionTable.row3.abRow3.text.toString())) correctAwnsers++
+        if (checkRowConjunction(conjunctionTable.row4.aRow4.text.toString() ,  conjunctionTable.row4.bRow4.text.toString(), conjunctionTable.row4.abRow4.text.toString())) correctAwnsers++
 
         toastMessage(correctAwnsers)
     }
 
 
     /**
-     * Checks the columns with letters and what gives a boolean based on that
-     * example : T^T = true; F^F = false; T^F = false
+     * Checks if the awnser is right
      */
-    fun checkRowConjunction(columnLeft: String, columnRight: String): Boolean {
-        if (columnLeft == columnRight && columnLeft == isTrue) return true
-        return false
+    fun checkRowConjunction(columnLeft: String, columnRight: String, awnser: String): Boolean {
+        if (awnser.toLowerCase() == isTrue.toLowerCase()){      // check if the awnser is True
+            if (columnLeft == columnRight){                     // if awnser is true check if column left == column right. Checkt dus op T^T
+                return true                                     // if that is true than return true
+            }
+        } else if (awnser.toLowerCase() == isFalse.toLowerCase()){  // else check if awnser == F.
+            return true                                             // return true if awnser == F
+        }
+//        toastMessageInvalidAwnser()                               // wont display because normal toast will go over it
+
+        return false                                                // if awnser also doesn't == f awnser is wrong
     }
 
     /**
-     * Checks if the awnser is true or false
+     * Message with correct awnser
      */
-    fun checkAwnserRowConjunction(awnser: String): Boolean {
-        if (awnser.toLowerCase() == isTrue.toLowerCase()) return true
-        return false
-    }
-
     fun toastMessage(correctAwnsers: Int){
         Toast.makeText(this, getString(R.string.correctAwnsers, correctAwnsers), Toast.LENGTH_SHORT).show()
     }
+
+
+//    fun toastMessageInvalidAwnser(){
+//        Toast.makeText(this, R.string.wrongInput, Toast.LENGTH_LONG).show()
+//    }
 }
